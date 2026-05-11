4e0d84aebf95a7f78166b123ccc3d61d64ba4403
brenin35 <brenoca35@gmail.com>
2026-05-11 15:11:31 -0300

    feat: estrutura base controller

---
+++ backend/src/controllers/seat_requests.controller.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+export const router = Router();
+
+router.post('/', async (req, res) => {
+});
+
+router.get('/', async (req, res) => {
+    res.json([]);
+});
+
+router.get('/:id', async (req, res) => {
+});
+
+router.put('/:id', async (req, res) => {
+});
+
+router.delete('/:id', async (req, res) => {
+});
