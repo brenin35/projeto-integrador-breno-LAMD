2166e65eba25f53b5eddaba71fdd8f21bbeb3ff8
brenin35 <brenoca35@gmail.com>
2026-05-11 15:29:16 -0300

    feat: app entrypoint

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,20 @@
 import express from 'express'
+import { router as usersRouter } from './src/controllers/users.controller.js'
+import { router as tripsRouter } from './src/controllers/trips.controller.js'
+import { router as seatRequestsRouter } from './src/controllers/seat_requests.controller.js'
 
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/users', usersRouter)
+app.use('/trips', tripsRouter)
+app.use('/seat-requests', seatRequestsRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
